666892123ce070e210e7e1bbac18a8e4913a1ed4
Kenan Ege <61656752+KenanAegean@users.noreply.github.com>
2023-03-11 14:14:19 +0300

    Files Added

---
+++ Assets/Scripts/Collectable.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Collectable : MonoBehaviour
+{
+    enum ItemType { Coin, Health, Ammo, InventoryItem} //Creates an ItemType enum (drop down)
+    [SerializeField] private ItemType itemType;
+    [SerializeField] private string inventoryStringName;
+    [SerializeField] private Sprite inventorySprite;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If the player is touching me, print "Collect" in the console
+        if (collision.gameObject == NewPlayer.Instance.gameObject)
+        {
+            if (itemType == ItemType.Coin)
+            {
+                NewPlayer.Instance.coinsCollected += 1;
+            }
+            else if (itemType == ItemType.Health)
+            {
+                if (NewPlayer.Instance.health < 100)
+                {
+                    NewPlayer.Instance.health += 1;
+                }
+            }
+            else if (itemType == ItemType.Ammo)
+            {
+               
+            }
+            else if(itemType == ItemType.InventoryItem)
+            {
+                NewPlayer.Instance.AddInventoryItem(inventoryStringName, inventorySprite);
+            }
+            else
+            {
+                
+            }
+
+            NewPlayer.Instance.UpdateUI();
+            Destroy(gameObject);
+        }
+    }
+}
